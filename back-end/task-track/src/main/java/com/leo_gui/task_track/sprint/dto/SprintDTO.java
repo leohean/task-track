0ea593be57686e0df614e6d8ba95d38b19b470e7
Guilherme Caetano <guilherme.caetano112004@gmail.com>
2025-06-28 14:11:44 -0300

    Add properties to sprintDTO

--- back-end/task-track/src/main/java/com/leo_gui/task_track/sprint/dto/SprintDTO.java
+++ back-end/task-track/src/main/java/com/leo_gui/task_track/sprint/dto/SprintDTO.java
@@ -2,11 +2,18 @@ package com.leo_gui.task_track.sprint.dto;
 
 import com.leo_gui.task_track.userStory.dto.UserStoryDTO;
 
+import jakarta.persistence.Column;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record SprintDTO(
         String name,
         String description,
+        LocalDateTime createdAt,
+        Integer createdBy,
+        LocalDateTime lastUpdateAt,
+        Integer lastUpdateBy,
         List<UserStoryDTO> userStoriesDTO
 ) {
 }
