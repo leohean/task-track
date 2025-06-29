361e89a60e04e3f1885e239761468da6518a61ec
Guilherme Caetano <guilherme.caetano112004@gmail.com>
2025-06-29 20:10:30 -0300

    Add min resolution

--- front-end/task-control/src/app/app.component.ts
+++ front-end/task-control/src/app/app.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterOutlet, Router, Event, NavigationEnd } from '@angular/router';
 import { HeaderComponent } from "./components/template/header/header.component";
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { LoginComponent } from './components/views/login/login.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'task-control';
   isValidRoute = true;
+  showResolutionWarning = false;
+  currentResolution = '';
+
+  private resizeListener: (() => void) | null = null;
 
   constructor(private router: Router) {
     this.router.events.subscribe((event: Event) => {
@@ -23,6 +28,41 @@ export class AppComponent {
         this.isValidRoute = !(lastChild.component === PageNotFoundComponent || lastChild.component === LoginComponent);
       }
     });
+  }
+
+  ngOnInit() {
+    this.updateResolutionDisplay();
+    this.checkResolutionWarning();
+    
+    // Atualizar quando a janela for redimensionada
+    this.resizeListener = () => {
+      this.updateResolutionDisplay();
+      this.checkResolutionWarning();
+    };
+    
+    window.addEventListener('resize', this.resizeListener);
+  }
+
+  ngOnDestroy() {
+    if (this.resizeListener) {
+      window.removeEventListener('resize', this.resizeListener);
+    }
+  }
+
+  closeWarning() {
+    this.showResolutionWarning = false;
+  }
+
+  private updateResolutionDisplay() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    this.currentResolution = `${width}x${height}`;
+  }
+
+  private checkResolutionWarning() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    this.showResolutionWarning = width < 1600 || height < 800;
   }
 
   private getLastChild(route: any): any {
