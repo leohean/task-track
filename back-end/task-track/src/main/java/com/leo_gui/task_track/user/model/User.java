a29f0c2077ac086f6a91da0e5772f44e2d76c98f
Guilherme Caetano <guilherme.caetano112004@gmail.com>
2025-06-29 12:30:12 -0300

    Fix authorities

--- back-end/task-track/src/main/java/com/leo_gui/task_track/user/model/User.java
+++ back-end/task-track/src/main/java/com/leo_gui/task_track/user/model/User.java
@@ -7,6 +7,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.Collection;
 import java.util.List;
 
@@ -22,7 +24,7 @@ public class User implements UserDetails {
     private String email;
     private String password;
     private UserRole role;
-
+    
     public User() {}
 
     public User(String name, String email, String password, UserRole role) {
@@ -33,6 +35,7 @@ public class User implements UserDetails {
     }
 
     @Override
+    @JsonIgnore
     public Collection<? extends GrantedAuthority> getAuthorities() {
         if(this.role == UserRole.ADMIN)
             return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
